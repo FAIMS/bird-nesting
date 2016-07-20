452dea1b648cbbc2f76e610c64357e02b317259a
DoxasticFox <christian@fedarch.org>
2016-07-20 21:39:53 +1000

    Some more work on BN-15

--- proc/post.sh
+++ proc/post.sh
@@ -19,5 +19,22 @@ string="<input ref=\"Revisit\">"
 replacement="<input ref=\"Revisit\" faims_table=\"true\">"
 perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_schema.xml
 
+string="
+  addNavigationButton(\"duplicate\", new ActionButtonCallback() {
+    actionOnLabel() {
+      \"{Duplicate}\";
+    }
+    actionOn() {
+      if(!isNull(getUuid(tabgroup))) {
+          duplicateRecord(tabgroup);
+      } else {
+          showWarning(\"{Warning}\", \"{This_record_is_unsaved_and_cannot_be_duplicated}\");
+      }
+    }
+  }, \"primary\");
+"
+replacement=""
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
 rm ui_logic.bsh.original
 rm ui_schema.xml.original
