ebd443f8f1a1f0ddbc82e71c455654dd3782b837
DoxasticFox <doxasticfox@github.com>
2017-08-22 22:09:52 +1000

    initial commit BN-27

--- proc/post.sh
+++ proc/post.sh
@@ -1,51 +1,9 @@
 #!/usr/bin/env bash
 
-#cd data
-#tar -cvzf ../module/data.tar.gz *
-#cd ..
-
 cd module
-
-# I hate this regex so much. Anyway, what it does is match everything in the
-# function definition, including the name, parens and opening curly brace, but
-# excluding the closing curly brace. This allows us to stick a line right before
-# the closing curly brace.
-string="(new([a-zA-Z]+)\\(\\){((?!\\n}).)+)"
-replacement="\\1
-  inherit\\2Fields();"
-perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_logic.bsh
-
-string="(newNestMeasurement\\(\\){((?!\\n}).)+)"
-replacement="\\1
-  initNestStatus();"
-perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_logic.bsh
-
-string="<input ref=\"Revisit\">"
-replacement="<input ref=\"Revisit\" faims_table=\"true\">"
-perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_schema.xml
-
-string="
-  addNavigationButton(\"duplicate\", new ActionButtonCallback() {
-    actionOnLabel() {
-      \"{Duplicate}\";
-    }
-    actionOn() {
-      if(!isNull(getUuid(tabgroup))) {
-          duplicateRecord(tabgroup);
-      } else {
-          showWarning(\"{Warning}\", \"{This_record_is_unsaved_and_cannot_be_duplicated}\");
-      }
-    }
-  }, \"primary\");
-"
-replacement=""
-perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
 echo "
 .fixedheightfive {
   height: 250px;
 }
 " >> ui_styling.css
-
-rm ui_logic.bsh.original
-rm ui_schema.xml.original
---
+++ tests/ModuleUtil.java
@@ -0,0 +1,438 @@
+package au.org.intersect.faims.android.util;
+
+import android.view.View;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+public class ModuleUtil extends ModuleHelper {
+    /*
+        Type: tab group
+        Ref:  Actual_Day_of_Hatching
+     */
+    public static View get_ActualDayofHatching(Solo solo) {
+        String ref = "Actual_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Control
+     */
+    public static View get_Control(Solo solo) {
+        String ref = "Control";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Day_of_Lay
+     */
+    public static View get_DayofLay(Solo solo) {
+        String ref = "Day_of_Lay";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Login
+     */
+    public static View get_Login(Solo solo) {
+        String ref = "Login";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Nest
+     */
+    public static View get_Nest(Solo solo) {
+        String ref = "Nest";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Nest_Measurement
+     */
+    public static View get_NestMeasurement(Solo solo) {
+        String ref = "Nest_Measurement";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: tab
+        Ref:  Actual_Day_of_Hatching/Actual_Day_of_Hatching
+     */
+    public static View get_ActualDayofHatching_ActualDayofHatching(Solo solo) {
+        String ref = "Actual_Day_of_Hatching/Actual_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Control/Control
+     */
+    public static View get_Control_Control(Solo solo) {
+        String ref = "Control/Control";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Day_of_Lay/Day_of_Lay
+     */
+    public static View get_DayofLay_DayofLay(Solo solo) {
+        String ref = "Day_of_Lay/Day_of_Lay";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Login/Login
+     */
+    public static View get_Login_Login(Solo solo) {
+        String ref = "Login/Login";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Nest/Nest
+     */
+    public static View get_Nest_Nest(Solo solo) {
+        String ref = "Nest/Nest";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Nest_Measurement/Nest_Measurement
+     */
+    public static View get_NestMeasurement_NestMeasurement(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: button
+        Ref:  Actual_Day_of_Hatching/Actual_Day_of_Hatching/Cancel
+     */
+    public static View get_ActualDayofHatching_ActualDayofHatching_Cancel(Solo solo) {
+        String ref = "Actual_Day_of_Hatching/Actual_Day_of_Hatching/Cancel";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Actual_Day_of_Hatching/Actual_Day_of_Hatching/Set_Actual_Day_of_Hatching
+     */
+    public static View get_ActualDayofHatching_ActualDayofHatching_SetActualDayofHatching(Solo solo) {
+        String ref = "Actual_Day_of_Hatching/Actual_Day_of_Hatching/Set_Actual_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Control/Control/Add_New_Nest
+     */
+    public static View get_Control_Control_AddNewNest(Solo solo) {
+        String ref = "Control/Control/Add_New_Nest";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Control/Search/Search_Button
+     */
+    public static View get_Control_Search_SearchButton(Solo solo) {
+        String ref = "Control/Search/Search_Button";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Day_of_Lay/Day_of_Lay/Cancel
+     */
+    public static View get_DayofLay_DayofLay_Cancel(Solo solo) {
+        String ref = "Day_of_Lay/Day_of_Lay/Cancel";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Day_of_Lay/Day_of_Lay/Set_Day_of_Lay
+     */
+    public static View get_DayofLay_DayofLay_SetDayofLay(Solo solo) {
+        String ref = "Day_of_Lay/Day_of_Lay/Set_Day_of_Lay";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Nest/Nest/Add_Nest_Visit
+     */
+    public static View get_Nest_Nest_AddNestVisit(Solo solo) {
+        String ref = "Nest/Nest/Add_Nest_Visit";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Nest/Nest/Compute_Estimated_Day_of_Hatching
+     */
+    public static View get_Nest_Nest_ComputeEstimatedDayofHatching(Solo solo) {
+        String ref = "Nest/Nest/Compute_Estimated_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Nest/Nest/Nest_Completed
+     */
+    public static View get_Nest_Nest_NestCompleted(Solo solo) {
+        String ref = "Nest/Nest/Nest_Completed";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Nest/Nest/Set_Actual_Day_of_Hatching
+     */
+    public static View get_Nest_Nest_SetActualDayofHatching(Solo solo) {
+        String ref = "Nest/Nest/Set_Actual_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Nest/Nest/Set_Day_of_Lay
+     */
+    public static View get_Nest_Nest_SetDayofLay(Solo solo) {
+        String ref = "Nest/Nest/Set_Day_of_Lay";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Nest_Measurement/Nest_Measurement/Picture_Button_1
+     */
+    public static View get_NestMeasurement_NestMeasurement_PictureButton1(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Picture_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Nest_Measurement/Nest_Measurement/Picture
+     */
+    public static View get_NestMeasurement_NestMeasurement_Picture(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Picture";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Control/Search/Entity_Types
+     */
+    public static View get_Control_Search_EntityTypes(Solo solo) {
+        String ref = "Control/Search/Entity_Types";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Nest/Nest/Box_Status
+     */
+    public static View get_Nest_Nest_BoxStatus(Solo solo) {
+        String ref = "Nest/Nest/Box_Status";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Nest/Nest/Location_Name
+     */
+    public static View get_Nest_Nest_LocationName(Solo solo) {
+        String ref = "Nest/Nest/Location_Name";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Nest_Measurement/Nest_Measurement/Nest_Status
+     */
+    public static View get_NestMeasurement_NestMeasurement_NestStatus(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Nest_Status";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Actual_Day_of_Hatching/Actual_Day_of_Hatching/Actual_Day_of_Hatching
+     */
+    public static EditText get_ActualDayofHatching_ActualDayofHatching_ActualDayofHatching(Solo solo) {
+        String ref = "Actual_Day_of_Hatching/Actual_Day_of_Hatching/Actual_Day_of_Hatching";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Control/Search/Search_Term
+     */
+    public static EditText get_Control_Search_SearchTerm(Solo solo) {
+        String ref = "Control/Search/Search_Term";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Day_of_Lay/Day_of_Lay/Day_of_Lay
+     */
+    public static EditText get_DayofLay_DayofLay_DayofLay(Solo solo) {
+        String ref = "Day_of_Lay/Day_of_Lay/Day_of_Lay";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest/Nest/Actual_Day_of_Hatching
+     */
+    public static View get_Nest_Nest_ActualDayofHatching(Solo solo) {
+        String ref = "Nest/Nest/Actual_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest/Nest/Box_ID
+     */
+    public static EditText get_Nest_Nest_BoxID(Solo solo) {
+        String ref = "Nest/Nest/Box_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest/Nest/Day_of_Lay
+     */
+    public static View get_Nest_Nest_DayofLay(Solo solo) {
+        String ref = "Nest/Nest/Day_of_Lay";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest/Nest/Estimated_Day_of_Hatching
+     */
+    public static View get_Nest_Nest_EstimatedDayofHatching(Solo solo) {
+        String ref = "Nest/Nest/Estimated_Day_of_Hatching";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest/Nest/Nest_Completed_at
+     */
+    public static View get_Nest_Nest_NestCompletedat(Solo solo) {
+        String ref = "Nest/Nest/Nest_Completed_at";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest/Nest/Nest_ID
+     */
+    public static EditText get_Nest_Nest_NestID(Solo solo) {
+        String ref = "Nest/Nest/Nest_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest_Measurement/Nest_Measurement/Egg_Count
+     */
+    public static EditText get_NestMeasurement_NestMeasurement_EggCount(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Egg_Count";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest_Measurement/Nest_Measurement/Hatched_Chick_Count_Dead
+     */
+    public static EditText get_NestMeasurement_NestMeasurement_HatchedChickCountDead(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Hatched_Chick_Count_Dead";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest_Measurement/Nest_Measurement/Hatched_Chick_Count_Live
+     */
+    public static EditText get_NestMeasurement_NestMeasurement_HatchedChickCountLive(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Hatched_Chick_Count_Live";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest_Measurement/Nest_Measurement/Measurements
+     */
+    public static EditText get_NestMeasurement_NestMeasurement_Measurements(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Measurements";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest_Measurement/Nest_Measurement/Nest_Measurement_Nest_ID
+     */
+    public static View get_NestMeasurement_NestMeasurement_NestMeasurementNestID(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Nest_Measurement_Nest_ID";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Nest_Measurement/Nest_Measurement/Notes
+     */
+    public static EditText get_NestMeasurement_NestMeasurement_Notes(Solo solo) {
+        String ref = "Nest_Measurement/Nest_Measurement/Notes";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Control/Control/Existing_Nests
+     */
+    public static View get_Control_Control_ExistingNests(Solo solo) {
+        String ref = "Control/Control/Existing_Nests";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Control/Search/Entity_List
+     */
+    public static View get_Control_Search_EntityList(Solo solo) {
+        String ref = "Control/Search/Entity_List";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Login/Login/User
+     */
+    public static View get_Login_Login_User(Solo solo) {
+        String ref = "Login/Login/User";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: webview
+        Ref:  Nest/Nest/Click_Button_for_Automated_Date_Insertion_1
+     */
+    public static View get_Nest_Nest_ClickButtonforAutomatedDateInsertion1(Solo solo) {
+        String ref = "Nest/Nest/Click_Button_for_Automated_Date_Insertion_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+}
